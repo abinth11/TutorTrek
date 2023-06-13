72dae4ed951a77aa0e2146a01c54ca71df4868fc
Abin <abinth250@gmail.com>
2023-06-13 11:43:20 +0530

    error page updated

--- client/src/components/common/ErrorElement.tsx
+++ client/src/components/common/ErrorElement.tsx
@@ -1,26 +1,31 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
 
 const ErrorElement: React.FC = () => {
   const error: any = useRouteError();
 
   return (
-    <div className='error-page'>
-      <style media=''></style>
-      <div className='error-message'>
-        <h1>{error.status}</h1>
-        <h2>Oops! Page not found.</h2>
-        <p>
-          We couldn't find the page you were looking for. This might be because
-          you have typed the web address incorrectly, or the page you were
-          looking for may have been moved, updated, or deleted.
-        </p>
-        <a href='/' className='home-button'>
-          Home
-        </a>
-      </div>
-    </div>
-  );
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">{error.status}</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              to="/"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </Link>
+            <a href="#" className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 };
 
 export default ErrorElement;
