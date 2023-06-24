6457e8ad21b191ea0fd30e5b1b4eae980a913266
Abin <abinth250@gmail.com>
2023-06-24 15:23:08 +0530

    axios api set for list courses

--- client/src/components/students/pages/Course/ListCourse.tsx
+++ client/src/components/students/pages/Course/ListCourse.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import CourseCard from './CourseCard'
 const ListCourse:React.FC = () =>{
     return (
-        <div>
-            list course
+        <div className='p-10 m-5 ml-8 flex flex-wrap space-x-6 '>
+            <CourseCard/>
+            <CourseCard/>
+            <CourseCard/>
         </div>
     )
 }
