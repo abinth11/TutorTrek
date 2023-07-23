ec60c4f6ba58ff554cce2cbf53e9d9843e397674
Abin <abinth250@gmail.com>
2023-07-23 17:40:48 +0530

    channels coming soon

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -34,6 +34,7 @@ import DashHome from "./components/pages/studentDash/DashHome";
 import StudentsTab from "./components/pages/studentManagement/StudentsTab";
 import InstructorProfile from "./components/pages/instructors/InsructorProfile";
 import MyStudents from "./components/pages/instructors/MyStudents";
+import InstructorChannels from "./components/pages/channel/InstructorChannels";
      
 const LazyListCourse = lazy(
   () => import("./components/pages/Course/ListCourse")
@@ -205,8 +206,12 @@ const AppRouter = createBrowserRouter([
         element:<MyStudents/>
       },
       {
-        path:"view-profile",
+        path:"view-profile",  
         element:<InstructorProfile/>
+      },
+      {
+        path:"view-channels",
+        element:<InstructorChannels/>
       }
     ],
   },
